95fcf263ccb393b5323bdaed5e6885cddd5a4b4c
jasonyou1995 <jason.you1995@gmail.com>
2017-05-26 12:13:24 -0700

    Initial commit: hello

---
+++ C2/hello.py
@@ -0,0 +1,6 @@
+# I found The Data Science Handbook in May 26 2017, 10:36 AM after the Friday class of
+# Math 327 in the Engineering Library (UW) new books shelf.
+# I'm sure that this will change my life and the whole world that I will change ;)
+
+
+print('Hello Data Science World!')
